74489534610757f2d55219f3e732b57a5c186e5f
Peter Pae <paendabear@gmail.com>
2019-03-27 14:30:16 -0400

    redo part 2 electric boogaloo

--- obj.js
+++ obj.js
@@ -1,10 +1,23 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
 const loaf = {
 
     flour: 300,
     water: 210,
     hydration: function () {
-        return (this.water/this.flour)*100
+        return (this.water/this.flour)*100;
     }
+};
+
+const probTwo = {
+    'foo' : 1,
+    'bar': 2,
+    'fum' : 3,
+    'quux' : 4,
+    'spam' : 5
+};
+
+for (let x in probTwo){
+    console.log(`${x} ${probTwo[x]}`); 
 }
 
-console.log(loaf.hydration());
