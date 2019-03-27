1c0273ce36ff2839384b48ff3ecd58c65d29fbb2
Peter Pae <paendabear@gmail.com>
2019-03-27 11:27:38 -0400

    add objectDrills.ks

---
+++ objectDrills.js
@@ -0,0 +1,12 @@
+/* eslint-disable strict */
+function createMyObject() {
+  const myObject = {
+    'foo' : 'bar',
+    'answerToUniverse' : 42,
+    'olly olly' : 'oxen free',
+    'sayHello' : function () {
+      return 'hello';
+    } 
+  };
+  return myObject;
+}
